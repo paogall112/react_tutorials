d1c3ea7eb6087b8582f130d938d3fda1ec290548
PaoGall112 <shrimpystew889@gmail.com>
2024-07-27 18:33:12 +0800

    Added Add.js file

---
+++ App.js
@@ -0,0 +1,14 @@
+function MyButton(){
+    return(
+        <button>I'm a button</button>
+    );
+}
+
+export default function MyApp(){
+    return (
+        <div>
+            <h1>Welcome to my App</h1>
+            <MyButton />
+        </div>
+    );
+}
